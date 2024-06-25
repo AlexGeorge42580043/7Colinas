b1886aab23d0afe77f9b6ed64cdce4c4fc9da141
AlexGeorge42580043 <georgealex42580043@gmail.com>
2024-06-25 16:32:59 -0300

    Incorporacion de Fecha y Hora actual

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <ESP32Time.h>//
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include <WiFi.h>
 #include <WiFiManager.h>
 #include <Arduino.h>
@@ -5,6 +8,13 @@
 #include <WiFiClientSecure.h>
 #include <DNSServer.h>
 #include <secrets.h>
+
+    ESP32Time rtc;
+
+//DE ACA PARA ABAJO ESTA LO QUE TIENE QUE VER CON LA HORA ACTUAL
+    WiFiUDP ntpUDP;
+    NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", -10800, 60000); //configura la zona horaria
+
 
 // DE ACA PARA ABAJO ESTA LO QUE TIENE QUE VER CON FIRESTORE
     UserAuth user_auth(API_KEY, USUARIO_EMAIL, USUARIO_CONTRA, 3000);
@@ -26,23 +36,30 @@
     Firestore::Documents Docs; // es parte de la plataforma Firebase y se utiliza para interactuar con la base de datos Firestore.
 
 
-
 // DECLARACION DE VARIABLES
     float temperatura;
     unsigned long previousMillis = 0;
     unsigned long documentPreviousMillis = 0;
     const unsigned int documentCreationInterval = 3000;
+    String Fecha;
+    String Hora;
+
 
 void setup() {
 
-    Serial.begin(115200);
+// CONFIGURACION VELOCIDAD PUETO SERIE
+  Serial.begin(115200);
 
+// ACA COMIENZA LA CONFIGURACION DE RTC PARA OTENCION DE FECHA ACTUAL
+  configTime(-10800, 0, "europe.pool.ntp.org");
+  struct tm timeinfo;
+  if (getLocalTime(&timeinfo)){
+    rtc.setTimeStruct(timeinfo); 
+  }
 
 // DE ACA PARA ABAJO ESTA LO QUE TIENE QUE VER CON LA CONEXION WIFI
-    //Crea una instancia de WiFiManager
-    WiFiManager wifiManager;
-    // Esto puede ser útil durante las pruebas o cuando deseas borrar las configuraciones guardadas
-    //wifiManager.resetSettings();
+    WiFiManager wifiManager;//Crea una instancia de WiFiManager
+    //wifiManager.resetSettings();// Esto puede ser útil durante las pruebas o cuando deseas borrar las configuraciones guardadas
 
     //Conexion automatica a wifi
     bool res;
@@ -80,22 +97,34 @@ void loop() {
   
     app.loop();
     Docs.loop();
+    timeClient.update();
+
+    Serial.println(timeClient.getFormattedTime()); // Muestra hora en terminal
+    Serial.println(rtc.getDate()); //Muestra fecha en terminal
+    delay(1000);
 
     //Verifica si la aplicacion esta lista para usar
     //Verifica si ya pasaron 3 segundos de la ultima vez que se cargaron datos en la base
        if (app.ready() && documentPreviousMillis < millis() - documentCreationInterval){
-        documentPreviousMillis = millis();
+        documentPreviousMillis = millis(); //Actualiza la variable al valor actual de Millis
 
-        String documentPath = "Produccion/"; //Crea una coleccion llamada Produccion con un documento random en firebase
+        String documentPath = "Produccion/" + String(timeClient.getEpochTime()); //Crea una coleccion llamada Produccion con un documento random en firebase
 
-        temperatura = ++temperatura;
+        temperatura = ++temperatura; //PROVISORIO
+
+        Fecha = (rtc.getDate()); //Se almacena la fecha actual en la variable Fecha de Tipo string
+        Hora = (timeClient.getFormattedTime()); //Se almacena la hora actual en la variable Hora de Tipo string
 
         Values::DoubleValue temperaturaValue(temperatura);
         Values::DoubleValue tanqueValue(1);
+        Values::StringValue fechaValue(Fecha);
+        Values::StringValue horaValue(Hora);
 
         Document<Values::Value> 
         doc("temperatura", Values::Value(temperaturaValue)); //Crea una coleccion llamada Temperatura con el valor que toma de la medicion
         doc.add("Tanque", Values::Value(tanqueValue)); //Crea una coleccion llamada Tanque con el valor del numero de tanque al que se refiere la medicion
+        doc.add("Fecha", Values::Value (fechaValue)); //Crea una coleccion llamada Fecha con la fecha actual
+        doc.add("Hora", Values::Value (horaValue)); //Crea una coleccion llamada Hora con la hora actual
 
         Docs.createDocument(aClient, Firestore::Parent(FIREBASE_PROYECTO_ID), documentPath, DocumentMask(), doc, asyncCB, "Documento creado  \n");
   }
