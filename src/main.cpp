ca606a93f01ff2a9c6d3913e7d018c0c8dd19dd6
AlexGeorge42580043 <georgealex42580043@gmail.com>
2024-07-11 15:43:24 -0300

    control de medicion y publicaciones de temp.

--- src/main.cpp
+++ src/main.cpp
@@ -36,13 +36,24 @@
     Firestore::Documents Docs; // es parte de la plataforma Firebase y se utiliza para interactuar con la base de datos Firestore.
 
 
-// DECLARACION DE VARIABLES
-    float temperatura;
-    unsigned long previousMillis = 0;
-    unsigned long documentPreviousMillis = 0;
-    const unsigned int documentCreationInterval = 3000;
-    String Fecha;
-    String Hora;
+    void crearDocumento(float, String, String, int); //declaracion de la funcion que se encarga de crear un documento en firebase
+    float convertir_rango(int); //declaracion de la funcion que se encarga de llevar la temp al rango deseado
+
+// DECLARACION DE VARIABLES Y VECTORES
+    const int adcPins[] = {34, 35, 32, 33, 39, 36};  // Pines GPIO asociados al ADC1
+    int valores[6]; //Vector que almacena el valor (0 - 1024) leido por el adc
+    int tanqueNumero = 1; //numero del tanque que se esta leyendo la temperatura
+    int publicacionNumero = 1; //numero del tanque al que pertenece la publicacion que se debe realizar
+    unsigned long previousMillisLeer = 0; //sirve para calcular el tiempo que trascurre y compararlo para leer
+    unsigned long previousMillisPublicar = 0;  //sirve para calcular el tiempo que trascurre y compararlo para Publicar
+    const unsigned int intervalLeer = 3000; //Intervalo para leer (3seg)
+    const unsigned int intervalPublicar = 120000; //Intervalo para publicar (10min)
+    float tempTanque1; //aca se almacena la temperatura (-10 - 25) de el tanque
+    float tempTanque2;//aca se almacena la temperatura (-10 - 25) de el tanque
+    float tempTanque3;//aca se almacena la temperatura (-10 - 25) de el tanque
+    float tempTanque4;//aca se almacena la temperatura (-10 - 25) de el tanque
+    float tempTanque5;//aca se almacena la temperatura (-10 - 25) de el tanque
+    float tempTanque6;//aca se almacena la temperatura (-10 - 25) de el tanque
 
 
 void setup() {
@@ -91,6 +102,15 @@ void setup() {
     initializeApp(aClient, app, getAuth(user_auth), asyncCB, "authTask"); //Se usa para inicializar una aplicación de Firebase con la configuración proporcionada
     
     app.getApp<Firestore::Documents>(Docs);
+
+
+//CONFIGURA PARA AJUSTAR LA SENCIBILIDAD DEL ADC
+    for (int tanqueNumero = 0; tanqueNumero < 6; ++tanqueNumero) {
+        // Configura la atenuación para 11 dB (rango de 0-3.3V)
+        analogSetAttenuation(ADC_11db);
+        analogReadResolution(10);  // Resolución de 10 bits
+    }
+
 }
 
 void loop() {
@@ -103,30 +123,74 @@ void loop() {
     Serial.println(rtc.getDate()); //Muestra fecha en terminal
     delay(1000);
 
-    //Verifica si la aplicacion esta lista para usar
-    //Verifica si ya pasaron 3 segundos de la ultima vez que se cargaron datos en la base
-       if (app.ready() && documentPreviousMillis < millis() - documentCreationInterval){
-        documentPreviousMillis = millis(); //Actualiza la variable al valor actual de Millis
-
-        String documentPath = "Produccion/" + String(timeClient.getEpochTime()); //Crea una coleccion llamada Produccion con un documento random en firebase
-
-        temperatura = ++temperatura; //PROVISORIO
-
-        Fecha = (rtc.getDate()); //Se almacena la fecha actual en la variable Fecha de Tipo string
-        Hora = (timeClient.getFormattedTime()); //Se almacena la hora actual en la variable Hora de Tipo string
-
-        Values::DoubleValue temperaturaValue(temperatura);
-        Values::DoubleValue tanqueValue(1);
-        Values::StringValue fechaValue(Fecha);
-        Values::StringValue horaValue(Hora);
-
-        Document<Values::Value> 
-        doc("temperatura", Values::Value(temperaturaValue)); //Crea una coleccion llamada Temperatura con el valor que toma de la medicion
-        doc.add("Tanque", Values::Value(tanqueValue)); //Crea una coleccion llamada Tanque con el valor del numero de tanque al que se refiere la medicion
-        doc.add("Fecha", Values::Value (fechaValue)); //Crea una coleccion llamada Fecha con la fecha actual
-        doc.add("Hora", Values::Value (horaValue)); //Crea una coleccion llamada Hora con la hora actual
-
-        Docs.createDocument(aClient, Firestore::Parent(FIREBASE_PROYECTO_ID), documentPath, DocumentMask(), doc, asyncCB, "Documento creado  \n");
+   
+  //intervalLeer=3seg
+  if (millis() - previousMillisLeer >= intervalLeer){ 
+    previousMillisLeer = millis();
+    //intervalPublicar=10min
+    if (millis() - previousMillisPublicar >= intervalPublicar){
+      // Publica cada 3 seg si pasaron 10 min
+      switch (publicacionNumero){
+        case 1:
+          crearDocumento(tempTanque1, rtc.getDate(), timeClient.getFormattedTime(), publicacionNumero);
+          publicacionNumero=2;
+        break;
+        case 2:
+          crearDocumento(tempTanque2, rtc.getDate(), timeClient.getFormattedTime(), publicacionNumero);
+          publicacionNumero=3;
+          break;
+        case 3:
+          crearDocumento(tempTanque3, rtc.getDate(), timeClient.getFormattedTime(), publicacionNumero);
+          publicacionNumero=4;
+          break;
+        case 4:
+          crearDocumento(tempTanque4, rtc.getDate(), timeClient.getFormattedTime(), publicacionNumero);
+          publicacionNumero=5;
+          break;
+        case 5:
+          crearDocumento(tempTanque5, rtc.getDate(), timeClient.getFormattedTime(), publicacionNumero);
+          publicacionNumero=6;
+          break;
+        case 6:
+          crearDocumento(tempTanque6, rtc.getDate(), timeClient.getFormattedTime(), publicacionNumero);
+          publicacionNumero=1;
+          previousMillisPublicar = millis();
+          break;
+      }
+    }
+    //Lee los valores de temperatura cada 3seg
+      switch (tanqueNumero){
+      case 1:
+        valores[0] = 26;//analogRead(adcPins[tanqueNumero]);
+        tempTanque1 = convertir_rango(valores[0]);
+        tanqueNumero = 2;
+      break;
+      case 2:
+        valores[1] = 59; //analogRead(adcPins[tanqueNumero]);
+        tempTanque2 = convertir_rango(valores[1]);
+        tanqueNumero = 3;
+        break;
+      case 3:
+        valores[2] = 359;//analogRead(adcPins[tanqueNumero]);
+        tempTanque3 = convertir_rango(valores[2]);
+        tanqueNumero = 4;
+        break;
+      case 4:
+        valores[3] = 1006;//analogRead(adcPins[tanqueNumero]);
+        tempTanque4 = convertir_rango(valores[3]);
+        tanqueNumero = 5;
+        break;
+      case 5:
+        valores[4] = 396;//analogRead(adcPins[tanqueNumero]);
+        tempTanque5 = convertir_rango(valores[4]);
+        tanqueNumero = 6;
+        break;
+      case 6:
+        valores[5] = 958;//analogRead(adcPins[tanqueNumero]);
+        tempTanque6 = convertir_rango(valores[5]);
+        tanqueNumero = 1;
+        break;
+    }
   }
 }
 
@@ -149,6 +213,31 @@ void printResult(AsyncResult &aResult){
 
   if (aResult.available()){
     Firebase.printf("task: %s", aResult.uid().c_str());
-    //, payload : % s\n ", aResult.uid().c_str(), aResult.c_str());
   }
 }
+
+float convertir_rango(int valor_ADC) {
+    float m = (25.0 - (-10.0)) / 1023.0;  // Pendiente
+    float b = 25.0 - m * 1023.0;  // Ordenada al origen
+    return m * valor_ADC + b;
+}
+
+void crearDocumento(float temperatura, String fecha, String hora, int tanque){
+    //Verifica si la aplicacion esta lista para usar
+    if (app.ready()){
+        String documentPath = "Produccion/" + String(timeClient.getEpochTime()); //Crea una coleccion llamada Produccion con un documento random en firebase
+
+        Values::DoubleValue temperaturaValue(temperatura);
+        Values::IntegerValue tanqueValue(tanque);
+        Values::StringValue fechaValue(fecha);
+        Values::StringValue horaValue(hora);
+
+        Document<Values::Value> 
+        doc("Temperatura", Values::Value(temperaturaValue)); //Crea una coleccion llamada Temperatura con el valor que toma de la medicion
+        doc.add("Tanque", Values::Value(tanqueValue)); //Crea una coleccion llamada Tanque con el valor del numero de tanque al que se refiere la medicion
+        doc.add("Fecha", Values::Value (fechaValue)); //Crea una coleccion llamada Fecha con la fecha actual
+        doc.add("Hora", Values::Value (horaValue)); //Crea una coleccion llamada Hora con la hora actual
+
+        Docs.createDocument(aClient, Firestore::Parent(FIREBASE_PROYECTO_ID), documentPath, DocumentMask(), doc, asyncCB, "Documento creado  \n");
+    }
+}   
